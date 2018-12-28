d66d32c6d3352fed96f953741d811ed78d0e0b38
JoeAlv <JoeAlv@DESKTOP-A8C1L5H>
2018-12-28 09:54:46 -0600

    #0953am-281218 Author: JoeAlv $m = En este commit se incluye la configuración de la base de datos Heroku PostgreSql en el script de conn DB.

--- apps/MyCCTV/assets/funcs/conn_db_mycctv.php
+++ apps/MyCCTV/assets/funcs/conn_db_mycctv.php
@@ -1,8 +1,13 @@
 <?php
 
-	$mysqli=new mysqli("localhost","root","AN3kbzshMq0iUQrx","mycctv"); //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
-
-	if(mysqli_connect_errno()){
+	//$mysqli=new mysqli("localhost","root","AN3kbzshMq0iUQrx","mycctv"); //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
+        
+        //$mysqli=new mysqli("ec2-54-225-150-216.compute-1.amazonaws.com","lxedzgjpgqoxuo","0134a1709b725c74b6ff4f0e8e14d8e72d080eb83dbfabe6e6e9d3b2f79777f7","dboib1mgrsnqa2");
+	$db = parse_url(getenv("postgres://lxedzgjpgqoxuo:0134a1709b725c74b6ff4f0e8e14d8e72d080eb83dbfabe6e6e9d3b2f79777f7@ec2-54-225-150-216.compute-1.amazonaws.com:5432/dboib1mgrsnqa2
+"));
+        $db["path"] = ltrim($db["path"], "/");
+        
+    if(mysqli_connect_errno()){
 		echo 'Conexion Fallida : ', mysqli_connect_error();
 		exit();
 	}
--- index.php
+++ index.php
@@ -2,27 +2,24 @@
 <!-- Author: TecnicoaCR -->
 <!DOCTYPE html>
 <html lang="es">
-  <head>
-		<?php
-      include 'assets/partials/head.php';
-    ?>
-    <title>Inicio - TecnicoaCR </title>
-  </head>
-  <body>
-		<!-- ---------- HEADER ---------- -->
-    <?php include 'assets/partials/header.php'?>
+    <head>
+        <?php include 'assets/partials/head.php'; ?>
+        <title>Inicio - TecnicoaCR </title>
+    </head>
+    <body>
+        <!-- ---------- HEADER ---------- -->
+        <?php include 'assets/partials/header.php'?>
+        <form action="apps/MyCCTV/login.php" method="get">
+            Login <input type="submit" value="Ingresar"/>
+        </form>
+        
+        <form action="sendEmail.php" method="get">
+            EmailTest<input type="submit" value="Ingresar"/>
+        </form>
 
-		<form action="apps/MyCCTV/login.php" method="get">
-      Login<input type="submit" value="Ingresar"/>
-    </form>
-
-    <form action="sendEmail.php" method="get">
-      EmailTest<input type="submit" value="Ingresar"/>
-    </form>
-
-		<!-- -------- FOOTER -------- -->
-	  <?php include 'assets/partials/footer.php'?>
-	</body>
+	<!-- -------- FOOTER -------- -->
+	<?php include 'assets/partials/footer.php'?>
+    </body>
 </html>
 <!-- Author: TecnicoaCR -->
 <?php //$Author: 'TecnicoaCR'; ?>
