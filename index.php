c66708e901f36b3372b7fbf02ef057d538e75e93
JoeAlv <JoeAlv@DESKTOP-A8C1L5H>
2019-01-06 00:08:59 -0600

    #1208pm-060119 Author: JoeAlv $msg = Cambios en el index

--- index.php
+++ index.php
@@ -83,16 +83,15 @@
 
                                         <div class="menu_social_container menu_mm">
                                                 <ul class="menu_social menu_mm">
-                                                        <li class="menu_social_item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                                                        <li class="menu_social_item"><a href="#"><i class="fab fa-instagram"></i></a></li>
-                                                        <li class="menu_social_item"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                                                        <li class="menu_social_item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                                        <li class="menu_social_item menu_mm"><a href="#"><i class="fab fa-pinterest"></i></a></li>
+                                                        <li class="menu_social_item menu_mm"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                                        <li class="menu_social_item menu_mm"><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                                        <li class="menu_social_item menu_mm"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                                        <li class="menu_social_item menu_mm"><a href="#"><i class="fab fa-twitter"></i></a></li>
                                                 </ul>
                                         </div>
 
-                                        <div class="menu_copyright menu_mm">
-                                            <span>Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Desarrollado con <i class="fa fa-heart" aria-hidden="true"></i> por <a href="https:/facebook.com/TecnicoaCR" target="_blank">TecnicoaCR</a></span>
-                                        </div>
+                                        <div class="menu_copyright menu_mm">Colorlib All rights reserved</div>
                                 </div>
 
                         </div>
