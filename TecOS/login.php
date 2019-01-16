5a3c027a2f12dbf0268fe63eca60f99317c28927
JoeAlv <JoeAlv@DESKTOP-A8C1L5H>
2019-01-16 17:18:44 -0600

    #1658pm-150119 Author: JoeAlv $msg = Se incluye sd-admin y dependencias del login

--- TecOS/login.php
+++ TecOS/login.php
@@ -49,9 +49,9 @@
             <title>Login - TecOS</title>
         
             <!-- Latest compiled and minified CSS -->
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+            <link rel="stylesheet" href="assets/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
             <!-- CSS  -->
-            <link href="site-data/assets/css/login.css" type="text/css" rel="stylesheet" media="screen,projection"/>     
+            <link href="assets/css/login.css" type="text/css" rel="stylesheet" media="screen,projection"/>
         </head>
         <body>
             <div class="container">
@@ -101,7 +101,7 @@
             
         </div><!-- /card-container -->
     </div><!-- /container -->
-		<?php include 'site-data/assets/footer.php' ?>
+		<?php include 'assets/footer.php' ?>
 	</body>
 </html>
 <?php
