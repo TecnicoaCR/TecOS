165727b89071dec41f6be2fb2db728b344ea7c58
JoeAlv <JoeAlv@DESKTOP-A8C1L5H>
2019-01-20 21:20:59 -0600

    #2120pm-150119 Author: JoeAlv $msg = Actualización de slider

--- TecOS/admin/site-data/assets/header.php
+++
@@ -1,8 +0,0 @@
-<?php //$Author: 'TecnicoaCR'; ?>
-<!-- Author: TecnicoaCR -->
-<header>
-  <a href="https://www.facebook.com/TecnicoaCR/" target="_blank"><img id="logo" src="assets/img/tclogo.png"/></a>
-  <h2>Bienvenido</h2> 
-</header>
-<?php //$Author: 'TecnicoaCR'; ?>
-<!-- Author: TecnicoaCR -->
--- TecOS/admin/site-data/dashboard/logout.php
+++
@@ -1,8 +0,0 @@
-<!-- Author: TecnicoaCR -->
-<?php
-// $Author: 'TecnicoaCR';
-  session_start();
-  session_unset();
-  session_destroy();
-  header('Location: ../login.php');
- ?>
